bb91c84e0b5764b49cdfea7d144f8bd714b9cefa
Nguyen Van Hong <hongbeubeu@gmail.com>
2019-08-09 17:21:52 +0700

    third commit

--- system/config/config.php
+++ system/config/config.php
@@ -2,7 +2,4 @@
 	define('DB_HOST', 	  'localhost');
 	define('DB_USER',     'root');
 	define('DB_PASSWORD', '');
-
-	define('CONTROLLER_DEFAULT', 'index');
-	define('ACTION_DEFAULT', 	 'index');
  ?>
